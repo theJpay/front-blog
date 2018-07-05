5fbdca9c3b90cb41eff1dec8ae56360961940214
Jules Paris <jparis@oui.sncf>
2018-07-05 12:17:18 +0200

    redirection vers /home si la route n'existe pas

---
+++ src/views/BlogArticle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>I will display my blog articles here</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Article",
+  created() {
+    this.$router.push("/home");
+  }
+};
+</script>
+
+<style scoped>
+</style>
